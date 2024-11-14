d6a47aac8015e3df8ae5d7ea0ec4582db6401bd5
Cameron Kelly <nopokelly@gmail.com>
2024-11-14 03:23:31 -0600

    added txt file for server ip address

--- client/Client.java
+++ client/Client.java
@@ -1,9 +1,13 @@
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Client {
     private static int SERVER_PORT = 8193;
 
+    private String server_ip;
     private Socket socket;
 
     public static void main(String args[]) throws IOException {
@@ -14,8 +18,10 @@ public class Client {
     }
 
     public void connect() throws IOException {
+        server_ip = new String(Files.readAllBytes(Paths.get("serverip.txt")), StandardCharsets.UTF_8);
+
         try {
-            socket = new Socket("localhost", SERVER_PORT);
+            socket = new Socket(server_ip, SERVER_PORT);
         } catch (ConnectException e) {
             System.out.println("Could not connect to server");
             return;
